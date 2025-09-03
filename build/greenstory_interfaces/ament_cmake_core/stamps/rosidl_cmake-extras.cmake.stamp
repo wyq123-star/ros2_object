df333c185260e390c581fa492a4b17f100b0eea0
yuqin wang <3186831775@qq.com>
2025-09-03 12:13:02 +0800

    删除冗余内容，重新构建硬件的驱动

--- build/greenstory_interfaces/ament_cmake_core/stamps/rosidl_cmake-extras.cmake.stamp
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(greenstory_interfaces_IDL_FILES "srv/TellStory.idl;msg/ListenStory.idl")
-set(greenstory_interfaces_INTERFACE_FILES "srv/TellStory.srv;srv/TellStory_Request.msg;srv/TellStory_Response.msg;msg/ListenStory.msg")
--- build/greenstory_interfaces/greenstory_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yutou/ros2_ws/build/greenstory_interfaces/greenstory_interfaces__py
-# Build directory: /home/yutou/ros2_ws/build/greenstory_interfaces/greenstory_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
