814b9fa5c20498ac1164beaffb359589610fc53b
yuqin wang <3186831775@qq.com>
2025-09-04 11:36:26 +0800

    编写完py订阅者

---
+++ install/base/lib/python3.10/site-packages/base/subscribe.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class Py_subscriber(Node):
+    def __init__(self):
+        super().__init__('py_subscriber')
+        self.subscriber_ = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.get_logger().info("Subscriber has been started.")
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    py_subscriber = Py_subscriber()
+
+    rclpy.spin(py_subscriber)
+
+    py_subscriber.destroy_node()
+    rclpy.shutdown()
---
+++ src/base/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'base'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yutou',
+    maintainer_email='3186831775@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "subscriber = base.subscribe:main",
+        ],
+    },
+)
