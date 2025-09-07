38e9da913db40dc03253f61a535e900472037f30
yuqin wang <3186831775@qq.com>
2025-09-07 15:47:32 +0800

    加入ms200p的驱动以及环境的source

--- src/all/launch/bag_record.launch.py
+++ src/all/launch/bag_record.launch.py
@@ -1,35 +1,42 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration # 导入LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
 
-    bag_save_path = DeclareLaunchArgument(
+    bag_save_path_arg = DeclareLaunchArgument(
         'bag_save_path',
-        default_value='my_bag1',
+        default_value='my_bag', # 默认路径
         description='Path to save the recorded bag files'
     )
 
-    bag_path_value = LaunchConfiguration('bag_save_path')
 
-    imu_recorder_node = Node(
+    mutli_recorder_node = Node(
         package='bags_record',
-        executable='imu_recorder',
-        name='imu_recorder',
+        executable='multi_recorder', # 请确保此可执行文件名与你的CMakeLists.txt中设置的一致
+        name='multi_bag_recorder',
         output='screen',
-        parameters=[{'bag_file_path': bag_path_value}] 
+        parameters=[{'bag_file_path': LaunchConfiguration('bag_save_path')}] # 正确引用启动参数
     )
-    lidar_recorder_node = Node(
-        package='bags_record',
-        executable='lidar_recorder',
-        name='lidar_recorder',
-        output='screen',
-        parameters=[{'bag_file_path': bag_path_value}]
+
+    mid360_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('livox_ros_driver2'), # 替换为你的包名
+                'launch_ROS2',
+                'rviz_MID360_launch.py' # 替换为你的launch文件名
+            )
+        )
     )
 
     return LaunchDescription([
-        bag_save_path,
-        imu_recorder_node,
-        lidar_recorder_node
+        bag_save_path_arg, # 必须将参数声明也添加到LaunchDescription中
+        mutli_recorder_node,
+        mid360_launch
     ])
