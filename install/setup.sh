317c561debdfb655307b544ad395edc8f8759a7d
yuqin wang <3186831775@qq.com>
2025-09-17 15:38:31 +0800

    重新编译ros2包，避免引入ros1环境

--- install/setup.sh
+++ install/setup.sh
@@ -31,27 +31,15 @@ _colcon_prefix_chain_sh_source_script() {
 
 # source chained prefixes
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/opt/ros/noetic"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/yutou/ros2_ws/packages/lidar_ros_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/yutou/ros2_ws/packages/ws_livox/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/yutou/ros2_ws/packages/ros_bridge_ws/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
 COLCON_CURRENT_PREFIX="/home/yutou/ros2_ws/packages/AIRY/install"
+_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
+
+# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
+COLCON_CURRENT_PREFIX="/home/yutou/ros2_ws/packages/lidar_ros_ws/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 
--- src/all/launch/run.launch.py
+++ src/all/launch/run.launch.py
@@ -7,12 +7,12 @@ import os
 
 def generate_launch_description():
 
-    ros1_bridge_node = Node(
-        package='ros1_bridge',
-        executable='dynamic_bridge',
-        name='ros1_bridge_node',
-        arguments=['--bridge-all-topics'],
-    )
+    # ros1_bridge_node = Node(
+    #     package='ros1_bridge',
+    #     executable='dynamic_bridge',
+    #     name='ros1_bridge_node',
+    #     arguments=['--bridge-all-topics'],
+    # )
 
     airy_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -24,13 +24,23 @@ def generate_launch_description():
         )
     )
 
-    # 简单地延迟启动 ros1_bridge_node
-    delayed_bridge = TimerAction(
-        period=5.0, # 延迟5秒。这个时间需要根据rslidar_sdk实际启动时间调整
-        actions=[ros1_bridge_node]
+    ms200_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('oradar_lidar'),
+                'launch',
+                'ms200_scan.launch.py',
+            )
+        )
     )
+    # 简单地延迟启动 ros1_bridge_node
+    # delayed_bridge = TimerAction(
+    #     period=5.0, # 延迟5秒。这个时间需要根据rslidar_sdk实际启动时间调整
+    #     actions=[ros1_bridge_node]
+    # )
 
     return LaunchDescription([
         airy_launch,
-        delayed_bridge,
+        # delayed_bridge,
+        ms200_launch,
     ])
